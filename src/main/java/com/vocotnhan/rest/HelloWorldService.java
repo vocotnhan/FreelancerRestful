5dae8b0e9a60cf7baf463bb95ecbb5d37a6a07ef
vocotnhan <vn112211@gmail.com>
2015-05-10 02:01:12 +0700

    Restservicee
    new

---
+++ src/main/java/com/vocotnhan/rest/HelloWorldService.java
@@ -0,0 +1,32 @@
+package com.vocotnhan.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+ 
+@Path("/hello")
+public class HelloWorldService {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Jersey say : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+	@GET
+	@Path("CheckStatus")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getStatus(@QueryParam("ProductLink") String productLink){
+		
+		return productLink;
+		
+	}
+ 
+}
